0ac87cd0cd1dc8b6c3eef33ea761b4bbd460d66e
bellyrose831 <145377757+bellyrose831@users.noreply.github.com>
2026-03-20 19:13:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#source: https://ttsmp3.com/
+#source: https://ttsmp3.com/, 
+#source: https://render.com/docs/deploy-flask
+#source: https://dashboard.render.com/web/srv-d6uu461aae7s73f7qmog/deploys/dep-d6uu46haae7s73f7qn10
 
 #python -m venv flaskvowels
 #flaskvowels/Scripts/activate
 #pip install flask
-#run using "python main.py"
+#run using "python app.py"
 
 from flask import Flask, render_template
 
